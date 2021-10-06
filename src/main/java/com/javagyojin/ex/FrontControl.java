eb63c4685890bcfee10503a8eeb9de0970330dde
ici-903-00 <ici-903-00@DESKTOP-9H8V5DQ>
2021-10-06 12:51:57 +0900

    Front Controller ex v1.0

---
+++ src/main/java/com/javagyojin/ex/FrontControl.java
@@ -0,0 +1,61 @@
+package com.javagyojin.ex;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FrontControl
+ */
+@WebServlet("*.do")//확장자 패턴으로 매핑->모든 클라이언트 요청이 FrontControl로 집중됨
+public class FrontControl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FrontControl() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		actionDo(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		actionDo(request, response);
+	}
+	
+	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		 //System.out.println("프론트 컨트롤러가 실행됨");
+		
+		String uri = request.getRequestURI();
+		//System.out.println(uri);
+		String conPath = request.getContextPath();
+		//System.out.println(conPath);
+		String command = uri.substring(conPath.length());
+		
+		if(command.equals("/insert.do")) {
+			System.out.println("insert command 호출");
+		} else if(command.equals("/delete.do")) {
+			System.out.println("delete command 호출");
+		} else if(command.equals("/update.do")) {
+			System.out.println("update command 호출");
+		}
+	}
+}
---
+++ src/main/webapp/frontControllerEx.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>frontControllerEx.jsp</title>
+</head>
+<body>
+	<a href="insert.do">insert 호출</a><br>
+	<a href="delete.do">delete 호출</a><br>
+	<a href="update.do">update 호출</a><br>
+</body>
+</html>
